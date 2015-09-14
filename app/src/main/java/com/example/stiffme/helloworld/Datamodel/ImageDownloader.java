c756f7369a9297fa68949b3915bff02c6fd1e7c3
unknown <stiffme@qq.com>
2015-09-14 13:26:08 +0800

    Add cache to bitmap img

--- app/src/main/java/com/example/stiffme/helloworld/Datamodel/ImageDownloader.java
+++ app/src/main/java/com/example/stiffme/helloworld/Datamodel/ImageDownloader.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.util.LruCache;
 
 import com.example.stiffme.helloworld.NetworkDef;
 
@@ -15,12 +16,22 @@ import java.net.URL;
  * Created by stiffme on 2015/9/13.
  */
 public class ImageDownloader extends AsyncTask<NoteListViewHolder,Void,Bitmap> {
+    private static LruCache<String,Bitmap> cache = new LruCache<String,Bitmap>(1024*1024*8) {
+        @Override
+        protected int sizeOf(String key, Bitmap value) {
+            return value.getRowBytes() * value.getHeight();
+        }
+    };
+
     NoteListViewHolder mHolder;
     @Override
     protected Bitmap doInBackground(NoteListViewHolder... params) {
         NoteListViewHolder holder = (NoteListViewHolder) params[0];
         mHolder = holder;
         try{
+            Bitmap cacheBit = cache.get(mHolder.note.img);
+            if(cacheBit !=null)
+                return cacheBit;
             URL url = new URL(NetworkDef.getImageUrl(holder.note.img));
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setConnectTimeout(1 * 1000);
@@ -28,6 +39,7 @@ public class ImageDownloader extends AsyncTask<NoteListViewHolder,Void,Bitmap> {
             if(conn.getResponseCode() == 200) {
                 InputStream is = conn.getInputStream();
                 Bitmap bitmap = BitmapFactory.decodeStream(is);
+                cache.put(holder.note.img,bitmap);
                 return bitmap;
             }
         } catch (Exception e)   {
